7efe93bca5a5fe1b59a7e7ab08ae94ea941ce83f
Ivan <vanyapopov2004@gmail.com>
2025-03-10 14:30:30 +0300

    +x for linux scripts

--- deployScript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-VERSION="1.0.0"
-
-echo "Deploying the project"
-docker build -t d4ylight/route-navigation-frontend:$VERSION -t d4ylight/route-navigation-frontend:latest . --target production-stage --no-cache
